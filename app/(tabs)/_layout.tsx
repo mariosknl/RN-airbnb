cc397b0cc61f8edca6f3b4e4a40a757325d9a5a5
mariosknl <mariosknl@outlook.com>
2024-04-29 11:00:58 +0300

    user profile with update and logout functionalities

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -60,6 +60,7 @@ const Layout = () => {
 				name="profile"
 				options={{
 					tabBarLabel: "Profile",
+					headerShown: false,
 					tabBarIcon: ({ color, size }) => (
 						<Ionicons name="person-circle-outline" color={color} size={size} />
 					),
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,19 +1,178 @@
-import { useAuth } from "@clerk/clerk-expo";
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import { useAuth, useUser } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
 import { Link } from "expo-router";
-import { View, Text, Button } from "react-native";
+import { useEffect, useState } from "react";
+import {
+	View,
+	Text,
+	Button,
+	StyleSheet,
+	SafeAreaView,
+	TouchableOpacity,
+	Image,
+	TextInput,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 const Profile = () => {
 	const { signOut, isSignedIn } = useAuth();
+	const { user } = useUser();
+	const [firstName, setFirstName] = useState(user?.firstName);
+	const [lastName, setLastName] = useState(user?.lastName);
+	const [email, setEmail] = useState(user?.emailAddresses[0].emailAddress);
+	const [edit, setEdit] = useState(false);
+
+	useEffect(() => {
+		if (!user) return;
+
+		setFirstName(user.firstName);
+		setLastName(user.lastName);
+		setEmail(user.emailAddresses[0].emailAddress);
+	}, [user]);
+
+	const onSaveUser = async () => {
+		try {
+			if (!firstName && !lastName) return;
+
+			await user?.update({
+				firstName,
+				lastName,
+			});
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setEdit(false);
+		}
+	};
+
+	const onCaptureImage = async () => {
+		const result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.Images,
+			allowsEditing: true,
+			quality: 0.75,
+			base64: true,
+		});
+
+		if (!result.canceled) {
+			const base64 = `data:image/png;base64,${result.assets[0].base64}`;
+			user?.setProfileImage({
+				file: base64,
+			});
+		}
+	};
 
 	return (
-		<View>
-			<Button title="Logout" onPress={() => signOut()} />
+		<SafeAreaView style={defaultStyles.container}>
+			<View style={styles.headerContainer}>
+				<Text style={styles.header}>Profile</Text>
+				<Ionicons name="notifications-outline" size={26} />
+			</View>
+
+			{user && (
+				<View style={styles.card}>
+					<TouchableOpacity onPress={onCaptureImage}>
+						<Image source={{ uri: user?.imageUrl }} style={styles.avatar} />
+					</TouchableOpacity>
+					<View style={{ flexDirection: "row", gap: 6 }}>
+						{!edit && (
+							<View style={styles.editRow}>
+								<Text style={{ fontFamily: "mon-b", fontSize: 22 }}>
+									{firstName} {lastName}
+								</Text>
+								<TouchableOpacity onPress={() => setEdit(true)}>
+									<Ionicons
+										name="create-outline"
+										size={24}
+										color={Colors.dark}
+									/>
+								</TouchableOpacity>
+							</View>
+						)}
+						{edit && (
+							<View style={styles.editRow}>
+								<TextInput
+									placeholder="First Name"
+									value={firstName || ""}
+									onChangeText={setFirstName}
+									style={[defaultStyles.inputField, { width: 100 }]}
+								/>
+								<TextInput
+									placeholder="Last Name"
+									value={lastName || ""}
+									onChangeText={setLastName}
+									style={[defaultStyles.inputField, { width: 100 }]}
+								/>
+								<TouchableOpacity onPress={onSaveUser}>
+									<Ionicons
+										name="checkmark-outline"
+										size={24}
+										color={Colors.dark}
+									/>
+								</TouchableOpacity>
+							</View>
+						)}
+					</View>
+					<Text>{email}</Text>
+					<Text>Since {user?.createdAt!.toLocaleDateString()}</Text>
+				</View>
+			)}
+
+			{isSignedIn && (
+				<Button title="Logout" onPress={() => signOut()} color={Colors.dark} />
+			)}
+
 			{!isSignedIn && (
-				<Link href={"/(modals)/login"}>
-					<Text>Login</Text>
+				<Link href={"/(modals)/login"} asChild>
+					<Button title="Log in" color={Colors.dark} />
 				</Link>
 			)}
-		</View>
+		</SafeAreaView>
 	);
 };
+
+const styles = StyleSheet.create({
+	headerContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		padding: 24,
+	},
+	header: {
+		fontFamily: "mon-b",
+		fontSize: 24,
+	},
+	card: {
+		backgroundColor: "#fff",
+		padding: 24,
+		borderRadius: 16,
+		marginHorizontal: 24,
+		marginTop: 24,
+		elevation: 2,
+		shadowColor: "#000",
+		shadowOpacity: 0.2,
+		shadowRadius: 6,
+		shadowOffset: {
+			width: 1,
+			height: 2,
+		},
+		alignItems: "center",
+		gap: 14,
+		marginBottom: 24,
+	},
+	avatar: {
+		width: 100,
+		height: 100,
+		borderRadius: 50,
+		backgroundColor: Colors.grey,
+	},
+	editRow: {
+		flex: 1,
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center",
+		gap: 8,
+	},
+});
+
 export default Profile;
